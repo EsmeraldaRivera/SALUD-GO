df095bc95baf9b739e74b20dd1a6e8dfe1b79c17
Esme <erivera5@estudiante.ibero.edu.co>
2026-05-23 14:09:02 -0500

    requiere

--- demo-vscode/index.js
+++ demo-vscode/index.js
@@ -1,14 +1,14 @@
-const express from 'express';
+const express = require('express');
 const app = express();  
 const port = 3000;
 
 app.use(express.json());
-app.listen (port, () => {
-console.log('server is running at http://Localhost:${port}');
 app.get('/', (req, res) => {
     res.send('hello, world');
-
-
+});
+app.listen (port, () => {
+console.log(`server is running at http://Localhost:${port}`);
+});
 
 
 
